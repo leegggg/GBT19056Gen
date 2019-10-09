8de6b312e66c3f1772484c3817a228a4baa9ed1c
LIN Yizhou <yizhou.lin@outlook.com>
2019-10-09 13:15:04 +0800

    dump

--- gbt19056/StandardVersion.go
+++ gbt19056/StandardVersion.go
@@ -1,8 +1,6 @@
 package gbt19056
 
 import (
-	"encoding/binary"
-
 	"github.com/leegggg/GBT19056Gen/utils/bcd"
 )
 
@@ -13,18 +11,11 @@ type StandardVersion struct {
 	Update HexUint8 `json:"update"`
 }
 
-// DumpDate ...
-func (e *StandardVersion) DumpDate() ([]byte, error) {
-	meta, err := e.dataBlockMeta.DumpDate()
-
+// DumpData StandardVersion
+func (e *StandardVersion) DumpData() ([]byte, error) {
 	bs := make([]byte, 2)
 	bs[0] = bcd.FromUint8(e.Year)
 	bs[1] = (byte)(e.Update)
-
-	// Data Length
-	dataLength := make([]byte, 4)
-	binary.BigEndian.PutUint32(dataLength, (uint32)(len(bs)))
-	meta = append(meta, dataLength...)
-	bs = append(meta, bs...)
-	return bs, err
+	bs, _ = e.linkDumpedData(bs)
+	return bs, nil
 }
--- gbt19056/recordModel.go
+++ gbt19056/recordModel.go
@@ -2,234 +2,17 @@ package gbt19056
 
 import (
 	"encoding/binary"
+	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/leegggg/GBT19056Gen/utils/bcd"
 )
 
 // HexUint8 ...
 type HexUint8 uint8
-
-// Status ...
-type Status [8]bool
-
-// SpeedStatus ...
-type SpeedStatus struct {
-	Speed  uint8  `json:"speed"`
-	Status Status `json:"status"`
-}
-
-// SpeedLogRecord ...
-type SpeedLogRecord struct {
-	Start         time.Time     `json:"start,string"`
-	SpeedStatuses []SpeedStatus `json:"speed_statuses"`
-}
-
-// PositionLogRecord ...
-type PositionLogRecord struct {
-	Start     string `json:"start"`
-	Positions []struct {
-		Speed    uint8    `json:"speed"`
-		Position Position `json:"position"`
-	} `json:"positions"`
-}
-
-// Position ...
-type Position struct {
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	Elevation float64 `json:"elevation"`
-}
-
-// AccidentLogRecord ...
-type AccidentLogRecord struct {
-	Ts            time.Time     `json:"ts,string"`
-	EndPosition   Position      `json:"position"`
-	SpeedStatuses []SpeedStatus `json:"speed_statuses"`
-}
-
-// OvertimeLogRecord ...
-type OvertimeLogRecord struct {
-	Start         time.Time `json:"start,string"`
-	End           time.Time `json:"end,string"`
-	PositionStart Position  `json:"position_start"`
-	PositionEnd   Position  `json:"position_end"`
-}
-
-// DriverLogRecord ...
-type DriverLogRecord struct {
-	Ts   time.Time `json:"ts,string"`
-	ID   string    `json:"id"`
-	Type HexUint8  `json:"type"` // 0x01: Login; 0x02: Logout; Other resversed
-}
-
-// ExternalPowerLogRecord ...
-type ExternalPowerLogRecord struct {
-	Ts   time.Time `json:"ts,string"`
-	Type HexUint8  `json:"type"` // 0x01: pluged; 0x02: unpluged;
-}
-
-// ConfigChangeLogRecord ...
-type ConfigChangeLogRecord struct {
-	Ts   time.Time `json:"ts,string"`
-	Type HexUint8  `json:"event"`
-}
-
-// SpeedStatusLogRecord ...
-type SpeedStatusLogRecord struct {
-	Status HexUint8  `json:"status"` // 0x01: normal; 0x02: error;
-	Start  time.Time `json:"start,string"`
-	End    time.Time `json:"end,string"`
-	Speeds []uint8   `json:"speeds"`
-}
-
-// dataBlockMeta ...
-type dataBlockMeta struct {
-	Code HexUint8 `json:"code"` // need decode from 0x01 ...
-	Name string   `json:"name"`
-}
-
-// DumpDate ...
-func (e *dataBlockMeta) DumpDate() ([]byte, error) {
-	bs := make([]byte, 19)
-	bs[0] = (byte)(e.Code)
-	sub := bs[1:]
-	name, _ := EncodeGBK(([]byte)(e.Name))
-	copy(sub, name)
-	return bs, nil
-}
-
-// DriverInfo ..
-type DriverInfo struct {
-	dataBlockMeta
-	ID string `json:"id"`
-}
-
-// RealTime ..
-type RealTime struct {
-	dataBlockMeta
-	Now time.Time `json:"now,string"`
-}
-
-// Odometer ..
-type Odometer struct {
-	dataBlockMeta
-	Now          time.Time `json:"now,string"`
-	TimeInit     time.Time `json:"time_init,string"`
-	MileageTotal uint32    `json:"mileage_total"`
-	MileageInit  uint32    `json:"mileage_init"`
-}
-
-// PulseFactor ..
-type PulseFactor struct {
-	dataBlockMeta
-	Now    time.Time `json:"now,string"`
-	Factor uint16    `json:"Factor"`
-}
-
-// VehicleInfo ..
-type VehicleInfo struct {
-	dataBlockMeta
-	ID        string `json:"id"`
-	Plate     string `json:"plate"`
-	PlateType string `json:"plate_type"`
-}
-
-// StatusSignalConfig ..
-type StatusSignalConfig struct {
-	dataBlockMeta
-	Now    time.Time `json:"now,string"`
-	Status Status    `json:"status"`
-	Config []string  `json:"config"`
-}
-
-// RecoderID ..
-type RecoderID struct {
-	dataBlockMeta
-	CCC     string    `json:"CCC"`
-	Version string    `json:"version"`
-	Dop     time.Time `json:"dop,string"`
-	Sn      uint32    `json:"sn"`
-	Comment string    `json:"comment"`
-}
-
-// SpeedLog ..
-type SpeedLog struct {
-	dataBlockMeta
-	Records []SpeedLogRecord `json:"records"`
-}
-
-// PositionLog ..
-type PositionLog struct {
-	dataBlockMeta
-	Records []PositionLogRecord `json:"records"`
-}
-
-// AccidentLog ..
-type AccidentLog struct {
-	dataBlockMeta
-	Records []AccidentLogRecord `json:"records"`
-}
-
-// OvertimeLog ..
-type OvertimeLog struct {
-	dataBlockMeta
-	Records []OvertimeLogRecord `json:"records"`
-}
-
-// DriverLog ..
-type DriverLog struct {
-	dataBlockMeta
-	Records []DriverLogRecord `json:"records"`
-}
-
-// ExternalPowerLog ..
-type ExternalPowerLog struct {
-	dataBlockMeta
-	Records []ExternalPowerLogRecord `json:"records"`
-}
-
-// ConfigChangeLog ..
-type ConfigChangeLog struct {
-	dataBlockMeta
-	Records []ConfigChangeLogRecord `json:"records"`
-}
-
-// SpeedStatusLog ..
-type SpeedStatusLog struct {
-	dataBlockMeta
-	Records []SpeedStatusLogRecord `json:"records"`
-}
-
-// ExportRecord ...
-type ExportRecord struct {
-	NumberBlock        uint16             `json:"number_block"`
-	StandardVersion    StandardVersion    `json:"standard_version"`
-	DriverInfo         DriverInfo         `json:"driver_info"`
-	RealTime           RealTime           `json:"real_time"`
-	Odometer           Odometer           `json:"odometer"`
-	PulseFactor        PulseFactor        `json:"pulse_factor"`
-	VehicleInfo        VehicleInfo        `json:"vehicle_info"`
-	StatusSignalConfig StatusSignalConfig `json:"status_signal_config"`
-	RecoderID          RecoderID          `json:"recoder_id"`
-	SpeedLog           SpeedLog           `json:"speed_log"`
-	PositionLog        PositionLog        `json:"position_log"`
-	AccidentLog        AccidentLog        `json:"accident_log"`
-	OvertimeLog        OvertimeLog        `json:"overtime_log"`
-	DriverLog          DriverLog          `json:"driver_log"`
-	ExternalPowerLog   ExternalPowerLog   `json:"external_power_log"`
-	ConfigChangeLog    ConfigChangeLog    `json:"config_change_log"`
-	SpeedStatusLog     SpeedStatusLog     `json:"speed_status_log"`
-}
-
-// DumpDate ...
-func (e *ExportRecord) DumpDate() ([]byte, error) {
-	bs := make([]byte, 2)
-	binary.BigEndian.PutUint16(bs, e.NumberBlock)
-	standardVersionDump, _ := e.StandardVersion.DumpDate()
-	bs = append(bs, standardVersionDump...)
-	return bs, nil
-}
 
 // UnmarshalJSON HexUint8 ...
 func (sd *HexUint8) UnmarshalJSON(input []byte) error {
@@ -244,24 +27,139 @@ func (sd *HexUint8) UnmarshalJSON(input []byte) error {
 	return nil
 }
 
-// UnmarshalJSON ...
-// func (s *SpeedStatusLog) UnmarshalJSON(data []byte) error {
-// 	type Alias SpeedStatusLog
-// 	aux := &struct {
-// 		*Alias
-// 	}{
-// 		Alias: (*Alias)(s),
-// 	}
-// 	if err := json.Unmarshal(data, &aux); err != nil {
-// 		return err
-// 	}
-// 	// s = SpeedStatusLog(*aux)
-// 	// s.SomeCustomType = time.Unix(aux.SomeCustomType, 0)
-// 	return nil
-// }
-// 	return nil
-// }
-// }
-// 	// s.SomeCustomType = time.Unix(aux.SomeCustomType, 0)
-// 	return nil
-// }
+// DateTime ...
+type DateTime struct {
+	time.Time
+}
+
+// DumpData DateTime
+func (e *DateTime) DumpData() ([]byte, error) {
+	bs := make([]byte, 6)
+	if e.Year() < 2000 || e.Year() > 2099 {
+		err := fmt.Errorf("Year %d not in range [2000-2099]", e.Year())
+		return bs, err
+	}
+
+	var nb uint8
+	nb = uint8(e.Year() - 2000)
+	bs[0] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Month())
+	bs[1] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Day())
+	bs[2] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Hour())
+	bs[3] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Minute())
+	bs[4] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Second())
+	bs[5] = bcd.FromUint8(nb)
+	return bs, nil
+}
+
+// DumpDataShort DateTime
+func (e *DateTime) DumpDataShort() ([]byte, error) {
+	bs := make([]byte, 3)
+	if e.Year() < 2000 || e.Year() > 2099 {
+		err := fmt.Errorf("Year %d not in range [2000-2099]", e.Year())
+		return bs, err
+	}
+
+	var nb uint8
+	nb = uint8(e.Year() - 2000)
+	bs[0] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Month())
+	bs[1] = bcd.FromUint8(nb)
+
+	nb = uint8(e.Day())
+	bs[2] = bcd.FromUint8(nb)
+
+	return bs, nil
+}
+
+// Status ...
+type Status [8]bool
+
+// DumpData DateTime
+func (e *Status) DumpData() (byte, error) {
+	var bs byte
+	bs = 0x00
+	if len(*e) > 8 {
+		return bs, fmt.Errorf("Status array too long")
+	}
+	// Set bitmap
+	for index, bit := range *e {
+		if bit {
+			bs |= 1 << index
+		}
+	}
+	return bs, nil
+}
+
+// SpeedStatus ...
+type SpeedStatus struct {
+	Speed  uint8  `json:"speed"`
+	Status Status `json:"status"`
+}
+
+// DumpData SpeedStatus
+func (e *SpeedStatus) DumpData() ([]byte, error) {
+	bs := make([]byte, 2)
+	bs[0] = e.Speed
+	status, _ := e.Status.DumpData()
+	bs[1] = status
+	return bs, nil
+}
+
+// Position ...
+type Position struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Elevation float64 `json:"elevation"`
+}
+
+// DumpData Position
+func (e *Position) DumpData() ([]byte, error) {
+	bs := make([]byte, 10)
+	positionMultiplier := 10000.0
+	latitude := int32(math.Round(e.Latitude * positionMultiplier))
+	longitude := int32(math.Round(e.Longitude * positionMultiplier))
+	elevation := int16(e.Elevation)
+
+	copy(bs[0:4], int32ToBytes(latitude))
+	copy(bs[4:8], int32ToBytes(longitude))
+	copy(bs[8:], int16ToBytes(elevation))
+	return bs, nil
+}
+
+// dataBlockMeta ...
+type dataBlockMeta struct {
+	Code HexUint8 `json:"code"` // need decode from 0x01 ...
+	Name string   `json:"name"`
+}
+
+// DumpDate ...
+func (e *dataBlockMeta) DumpData() ([]byte, error) {
+	bs := make([]byte, 19)
+	bs[0] = (byte)(e.Code)
+	sub := bs[1:]
+	name, _ := EncodeGBK(([]byte)(e.Name))
+	copy(sub, name)
+	return bs, nil
+}
+
+// linkDumpedData
+func (e dataBlockMeta) linkDumpedData(body []byte) ([]byte, error) {
+	meta, err := e.DumpData()
+	// Data Length
+	dataLength := make([]byte, 4)
+	binary.BigEndian.PutUint32(dataLength, (uint32)(len(body)))
+	meta = append(meta, dataLength...)
+	bs := append(meta, body...)
+	return bs, err
+}
